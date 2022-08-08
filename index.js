c81c92db4cd793ae35c6e9cef7f35db5d6947ada
Naija10 <83394508+Naija10@users.noreply.github.com>
2022-08-08 21:17:49 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,69 @@
+//Detecting button press
+
+var numberOfDrums = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfDrums; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    var buttonInnerHTML = this.innerHTML;
+    makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
+  });
+}
+
+
+//Detecting key press
+
+document.addEventListener("keydown", function(event) {
+  makeSound(event.key);
+  buttonAnimation(event.key);
+})
+
+function makeSound(key) {
+  switch (key) {
+    case "z":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "x":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "c":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+
+    case "v":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+
+    case "b":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+
+    case "n":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+
+    case "m":
+      var kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+    default:
+      console.log(buttonInnerHTML);
+
+  }
+}
+
+function buttonAnimation(currentKey) {
+  var activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout(function() {
+    activeButton.classList.remove("pressed");
+  }, 100);
+}
